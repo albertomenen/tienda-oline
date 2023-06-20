5d180738de76515fc0f4333c43bab5a441d261f2
Alberto <alberto.menendez89@gmail.com>
2023-06-20 19:54:24 +0200

    base-online-store

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Fuck Sapos",
+  description: "Esta es la tienda con mas flow de todo Madrid",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
---
+++ sanity/lib/client.ts
@@ -0,0 +1,11 @@
+import { createClient } from "next-sanity"
+
+import { apiVersion, dataset, projectId, useCdn } from "../env"
+
+export const client = createClient({
+  apiVersion,
+  dataset,
+  projectId,
+  useCdn,
+  token: "skRkOfYDplAkG0TbcQJdGPsfVcza4pNcc6og74dPHDWLbubYolFqJcJ76dabtZaxNkdaGh8LJsOIql8sZsAIz32gtoqEdqjPlwAvgJ569YmWjtQZnF4bOdDdgrCvvZXKnJEA0hSJnRHLBdnTrBpRss8HunmzgXlLnP0NBnQI8P3qJPRVSc4w",
+})
