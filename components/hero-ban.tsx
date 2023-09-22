095ecffb89639ffb76c46e2408fcec8d060e526a
Alberto <alberto.menendez89@gmail.com>
2023-09-22 11:18:45 +0200

    using new next version

--- components/hero-ban.tsx
+++ components/hero-ban.tsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
 import Image from "next/image";
 import { urlForImage } from "@/sanity/lib/image";
-
-import { SanityProduct } from "@/config/inventory";
 import { shimmer, toBase64 } from "@/lib/image";
-
-interface Props {
-  product: SanityProduct;
-}
 
 export function Hero() {
   const [selectedImage, setSelectedImage] = useState(0);
+
   return (
     <div className="flex flex-col-reverse">
-      {/* Main Image */}
       <div className="aspect-h-1 aspect-w-1 w-full">
         <Image
           priority
