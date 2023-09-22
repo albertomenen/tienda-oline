8921343ff311b6ba926621f1e4acb89fb3117438
Alberto <alberto.menendez89@gmail.com>
2023-09-22 10:36:45 +0200

    reverting some changes

--- components/hero-ban.tsx
+++ components/hero-ban.tsx
@@ -1,36 +1,32 @@
-"use client"
+import { useState } from "react";
+import Image from "next/image";
+import { urlForImage } from "@/sanity/lib/image";
 
-import { useState } from "react"
-import Image from "next/image"
-import { urlForImage } from "@/sanity/lib/image"
-
-import { SanityProduct } from "@/config/inventory"
-import { shimmer, toBase64 } from "@/lib/image"
+import { SanityProduct } from "@/config/inventory";
+import { shimmer, toBase64 } from "@/lib/image";
 
 interface Props {
-  product: SanityProduct
+  product: SanityProduct;
 }
 
 export function Hero() {
-
-    const [selectedImage, setSelectedImage ] = useState(0)
+  const [selectedImage, setSelectedImage] = useState(0);
   return (
     <div className="flex flex-col-reverse">
-
       {/* Main Image */}
       <div className="aspect-h-1 aspect-w-1 w-full">
         <Image
-         priority
+          priority
           src="/perfume-hero.png"
           alt={`Perfume Image`}
-         width={2176}
+          width={2176}
           height={544}
           quality={100}
-         placeholder="blur"
-         blurDataURL={`data: image/svg+xml; base64,${toBase64(shimmer(600,750))}`}
-         className="h-full w-full border-2 border-gray-200 object-cover object-center shadow-sm dark:border-gray-800 sm:rounded-lg"
-       />
-     </div>
-   </div>
-  )
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(600,750))}`}
+          className="h-full w-full border-2 border-gray-200 object-cover object-center shadow-sm dark:border-gray-800 sm:rounded-lg"
+        />
+      </div>
+    </div>
+  );
 }
